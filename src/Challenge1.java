15e100cd48d6c083108991df37eefec721f52b1b
Dev-JeanCharles <jeancduasan@gmail.com>
2024-01-23 14:51:35 -0300

    Challenge1 Complete - Multiples of 3 or 5

--- src/Challenge1.java
+++ src/Challenge1.java
@@ -1,5 +1,13 @@
+//Challenge 1 - Multiples of 3 or 5
 public class Challenge1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int somaNumeros = 0;
+        for (int i = 1; i < 1000; i++) {
+            if ((i % 3) == 0 || i % 5 == 0) {
+                System.out.println(i);
+                somaNumeros += i;
+            }
+        }
+        System.out.println("A soma dos múltiplos é: "+ somaNumeros);
     }
 }
